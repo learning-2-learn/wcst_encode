9e375bc5adbcbddd66297b2c23c6e49e8ebb07ac
Edgar Y. Walker <eywalker@uw.edu>
2023-04-03 12:45:08 -0700

    Update local setup

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-from os import path
-
-here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='wcst_encode',
-    version='0.0.0',
+    name="wcst_encode",
+    version="0.0.0",
     description='Tools and analysis for encoding behavioral/experimental variables from neural data',
-    author='Patrick Zhang',
-    author_email='pqz317@uw.edu',
-    packages=find_packages(exclude=[]),
+    packages=find_packages(),
 )
