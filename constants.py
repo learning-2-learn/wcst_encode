c7cc127d5274cd39a556172ea507d6bec582858c
Patrick Zhang <pqz317@uw.edu>
2023-04-05 00:44:51 +0000

    refactor into another notebook

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
+# useful constants during analysis
 
 FEATURES = [
     'CIRCLE', 'SQUARE', 'STAR', 'TRIANGLE', 
     'CYAN', 'GREEN', 'MAGENTA', 'YELLOW', 
     'ESCHER', 'POLKADOT', 'RIPPLE', 'SWIRL'
 ]
+
+NUM_UNITS = 59
+
+COLUMN_NAMES_W_UNITS = FEATURES + ["CORRECT", "INCORRECT"] + [f"unit_{i}" for i in range(0, NUM_UNITS)]
+COLUMN_NAMES = FEATURES + ["CORRECT", "INCORRECT"]
